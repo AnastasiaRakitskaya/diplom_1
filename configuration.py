c61a7b972e896e7a444dae3eeb6c19f2e54d7376
Anastasia Rakitskaya <rakitskaaanastasia6@gmail.com>
2025-08-17 19:03:30 +0300

    zaebalsya

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://525cba70-3fbd-4979-94ad-b9b45e7eb9ee.serverhub.praktikum-services.ru"
+CREATE_ORDER = "/api/v1/orders"
+GET_TRACK = "/api/v1/orders/track"
---
+++ request_data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "человек",
+    "lastName": "большой",
+    "address": "ленина",
+    "metroStation": 4,
+    "phone": "+79877437167",
+    "rentTime": 4,
+    "deliveryDate": "2025-08-20",
+    "comment": "привет",
+    "color": [
+        "BLACK"
+    ]
+}
